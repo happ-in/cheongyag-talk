08aca7cd1ab87245a1490f59b706b3311a4e8dd3
happ-in <gpqls304@naver.com>
2026-01-07 21:09:38 +0900

    Feat: BE 스켈레톤

---
+++ Backend/cheongyagtalk-api/src/main/java/com/sns/platform/cheongyagtalk_api/CheongyagtalkApiApplication.java
@@ -0,0 +1,13 @@
+package com.sns.platform.cheongyagtalk_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheongyagtalkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheongyagtalkApiApplication.class, args);
+	}
+
+}
---
+++ Backend/cheongyagtalk-api/src/test/java/com/sns/platform/cheongyagtalk_api/CheongyagtalkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sns.platform.cheongyagtalk_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheongyagtalkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
