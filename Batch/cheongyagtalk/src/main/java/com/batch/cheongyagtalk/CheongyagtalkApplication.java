e91dd96b4a3392b881f5a4a8eafd4bcccee9cc62
happ-in <gpqls304@naver.com>
2026-01-21 22:33:25 +0900

    Feat: 배치 스켈레톤코드 작성

---
+++ Batch/cheongyagtalk/src/main/java/com/batch/cheongyagtalk/CheongyagtalkApplication.java
@@ -0,0 +1,13 @@
+package com.batch.cheongyagtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheongyagtalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheongyagtalkApplication.class, args);
+	}
+
+}
